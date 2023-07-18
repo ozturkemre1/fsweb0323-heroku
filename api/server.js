35abb926e769fb667c91a2c9f5b4b273751e1500
Emre <ozturkemre@mail.com>
2023-07-18 14:46:19 +0300

    message changed

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const server = express();
 require("dotenv").config();
 
 server.get("/hello", (req,res) => {
-    res.json({message:process.env.MESSAGE || "yokmuş"})
+    res.json({message:process.env.MESSAGE || "Hello from command line"})
 })
 
 module.exports = server
